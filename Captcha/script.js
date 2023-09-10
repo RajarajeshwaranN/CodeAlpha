ccfe750988c22108c4ab9285c50971f995f74f7c
RajarajeshwaranN <122969400+RajarajeshwaranN@users.noreply.github.com>
2023-09-10 15:17:33 +0530

    Add files via upload

---
+++ Captcha/script.js
@@ -0,0 +1,11 @@
+alert("Welcome to my page!");
+function myFunction() {
+    let element = document.body;
+    element.classList.toggle("dark");
+    let btn=document.getElementsByClassName('btn')[0];
+    btn.classList.toggle('dark')
+}
+
+function button(){
+    alert("Ordered!!!!!");
+}
